25ea51b41f3d0c503eb1e20e8f005843496b9285
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-07-27 00:08:40 +0530

    feat: added logic

---
+++ script.js
@@ -0,0 +1,23 @@
+const userInput = document.querySelector('.user-input')
+const btn = document.querySelector('.btn')
+const outputBox = document.querySelector('.output-box')
+
+const serverUrl ='https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json'
+
+const generateTranslationUrl = (input) => `${serverUrl}?text=${input}`
+
+const clickHandler = async () => {
+    try{
+        const userInputText = userInput.value
+        const res = await fetch(generateTranslationUrl(userInputText))
+        const data = await res.json()
+        console.log(data)
+        outputBox.innerText = data.contents.translated
+    }catch(error){
+        console.log(error)
+        alert('Something Went Wrong!!! Please Try Again Later')
+    }
+}
+
+btn.addEventListener('click', clickHandler)
+    
